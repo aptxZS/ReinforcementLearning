16b7e473ca660ab9aa8d576a4847eb555ffc904d
dda410 <dimitri1215@gmail.com>
2019-05-20 01:43:21 +0200

    Implemented methods to calculate stick and follow indices

--- Coursework/mystrategies/EA2Player.java
+++ Coursework/mystrategies/EA2Player.java
@@ -17,35 +17,41 @@ public class EA2Player {
     double result = 0;
     for(int k = 1; k < numberOfPlays; k++) {
       double upperLimit = 11;
-      double x = Math.pow(smallGamma, -k) / bigGamma;
-      double diff = (upperLimit + history.get(k) + history.get(k-1)) % upperLimit;
+      double x = Math.pow(smallGamma, numberOfPlays - k) / bigGamma;
+      System.out.println("k: " + history.get(k) + " k-1: " + history.get(k-1));
+      double diff = (upperLimit + history.get(k) - history.get(k-1)) % upperLimit;
       double distance = diff < upperLimit - diff ? diff : upperLimit - diff;
-      result += x / Math.pow(distance, smallP);
+      System.out.println(distance);
+      result += x * Math.pow(distance, smallP);
     }
     stickIndex = -result;
   }
 
-  public void updateFollowIndeces(double smallGamma, double bigGamma, int numberOfPlays, double smallP, ArrayList<Integer> historyOther, ArrayList<Integer> history0) {
+  public void updateFollowIndices(double smallGamma, double bigGamma, int numberOfPlays, double smallP, ArrayList<Integer> historyOther, ArrayList<Integer> history0) {
     double followOtherResult = 0;
     double follow0Result = 0;
     double followIndexResult = 0;
     for(int k = 1; k < numberOfPlays; k++) {
       double upperLimit = 11;
-      double x = Math.pow(smallGamma, -k) / bigGamma;
+      double x = Math.pow(smallGamma, numberOfPlays - k) / bigGamma;
       // Updating follow player J index
-      double diffWithOther = (upperLimit + history.get(k) + historyOther.get(k-1)) % upperLimit;
-      double distanceWithOther = diffWithOther < upperLimit - diff ? diff : upperLimit - diff;
-      followOtherResult += x / Math.pow(distance, smallP);
+      double diffWithOther = (upperLimit + history.get(k) - historyOther.get(k-1)) % upperLimit;
+      double distanceWithOther = diffWithOther < upperLimit - diffWithOther ? diffWithOther : upperLimit - diffWithOther;
+      followOtherResult += x * Math.pow(distanceWithOther, smallP);
       // Updating follow player 0 index
-      double diffWith0 = (upperLimit + history.get(k) + history0.get(k-1)) % upperLimit;
-      double distanceWith0 = diffWith0 < upperLimit - diff ? diff : upperLimit - diff;
-      follow0Result += x / Math.pow(distance, smallP);
+      double diffWith0 = (upperLimit + history.get(k) - history0.get(k-1)) % upperLimit;
+      double distanceWith0 = diffWith0 < upperLimit - diffWith0 ? diffWith0 : upperLimit - diffWith0;
+      follow0Result += x * Math.pow(distanceWith0, smallP);
       // Updating general follow index
-      followIndexResult += x / Math.pow(Math.min(diffWithOther, diffWith0) , smallP);
+      followIndexResult += x * Math.pow(Math.min(distanceWithOther, distanceWith0) , smallP);
     }
     followOther = -followOtherResult;
     follow0 = -follow0Result;
     followIndex = -followIndexResult;
+  }
+
+  public String toString() {
+    return "Player {\n    stickIndex: " + stickIndex + "\n    followIndex: " + followIndex + "\n    followOther: " + followOther + "\n    follow0: " + follow0 + "\n    history: " + history + "\n}";
   }
 
 }
