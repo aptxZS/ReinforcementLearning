59106f40cebd8ed36913869d5caa580a29b97ac8
dda410 <dimitri1215@gmail.com>
2019-05-20 02:18:34 +0200

    Fixed follow index to calculate distance between current and opposite position of previous one

--- Coursework/mystrategies/EA2Player.java
+++ Coursework/mystrategies/EA2Player.java
@@ -7,16 +7,22 @@ public class EA2Player {
 
   double stickIndex, followIndex, followOther, follow0;
   ArrayList<Integer> history;
+  double upperLimit;
 
   public EA2Player() {
     stickIndex = followIndex = followOther = follow0 = 0;
     history = new ArrayList<Integer>();
+    upperLimit = 12;
+  }
+
+  private int oppositeLocation(int location) {
+    int half = (int) upperLimit / 2;
+    return location >=  half ? location - half : location + half;
   }
 
   public void updateStickIndex(double smallGamma, double bigGamma, int numberOfPlays, double smallP) {
     double result = 0;
     for(int k = 1; k < numberOfPlays; k++) {
-      double upperLimit = 11;
       double x = Math.pow(smallGamma, numberOfPlays - k) / bigGamma;
       System.out.println("k: " + history.get(k) + " k-1: " + history.get(k-1));
       double diff = (upperLimit + history.get(k) - history.get(k-1)) % upperLimit;
@@ -32,14 +38,13 @@ public class EA2Player {
     double follow0Result = 0;
     double followIndexResult = 0;
     for(int k = 1; k < numberOfPlays; k++) {
-      double upperLimit = 11;
       double x = Math.pow(smallGamma, numberOfPlays - k) / bigGamma;
       // Updating follow player J index
-      double diffWithOther = (upperLimit + history.get(k) - historyOther.get(k-1)) % upperLimit;
+      double diffWithOther = (upperLimit + history.get(k) - oppositeLocation(historyOther.get(k-1))) % upperLimit;
       double distanceWithOther = diffWithOther < upperLimit - diffWithOther ? diffWithOther : upperLimit - diffWithOther;
       followOtherResult += x * Math.pow(distanceWithOther, smallP);
       // Updating follow player 0 index
-      double diffWith0 = (upperLimit + history.get(k) - history0.get(k-1)) % upperLimit;
+      double diffWith0 = (upperLimit + history.get(k) - oppositeLocation(history0.get(k-1))) % upperLimit;
       double distanceWith0 = diffWith0 < upperLimit - diffWith0 ? diffWith0 : upperLimit - diffWith0;
       follow0Result += x * Math.pow(distanceWith0, smallP);
       // Updating general follow index
